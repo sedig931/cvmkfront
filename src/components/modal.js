e9a31b7dffbc39ee3ef68569e4fa8fb2d6797c4a
sedig931 <sedig931@gmail.com>
2024-11-25 15:19:08 +0300

    commit25/Nov

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,6 +1,7 @@
 
 // const SERVER_URL = 'http://localhost:300';
-const SERVER_URL = 'https://salesprojectb23.netlify.app/api';
+// const SERVER_URL = 'https://salesprojectb23.netlify.app/api';
+const SERVER_URL = 'https://samdtc931.com/api';
 
 export const addNewCustomer = async function (newCustomer) {
   try {
